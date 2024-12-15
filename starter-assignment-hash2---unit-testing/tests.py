ceb03edf47415b48488ffa985015d6d36c3da8ef
Manjil Rawal <113137093+manjilrawal007@users.noreply.github.com>
2024-12-15 18:23:09 -0500

    Update tests.py

--- starter-assignment-hash2---unit-testing/tests.py
+++ starter-assignment-hash2---unit-testing/tests.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 
-sys.path.append("/home/codio/workspace/") #have to tell the unittest the PATH to find boggle_solver.py and the Boggle Class
+# sys.path.append("/home/codio/workspace/") #have to tell the unittest the PATH to find boggle_solver.py and the Boggle Class
 
 from boggle_solver import Boggle
 
@@ -16,9 +16,72 @@ class TestSuite_Alg_Scalability_Cases(unittest.TestCase):
     solution = [x.upper() for x in solution]
     expected = ["abc", "abdhi", "cfi", "dea"];
     expected = [x.upper() for x in expected]
-		solution = sorted(solution)
+    solution = sorted(solution)		
     expected = sorted(expected)
     self.assertEqual(expected, solution)
+
+  # testing on a 4X4 grid
+  def test_4x4_grid(self):
+    grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"], ["G", "Z", "Qu", "R"], ["O", "N", "T", "A"]]
+    dictionary = ["art", "ego", "gent", "get", "net", "new", "newt", "prat", "pry", "qua", "quart", "quartz", 
+                  "rat", "tar", "tarp", "ten", "went", "wet", "arty", "not", "quar"]
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = ["ART", "EGO", "GENT", "GET", "NET", "NEW", "NEWT", "PRAT", "PRY", "QUA", "QUART", 
+                "QUARTZ", "RAT", "TAR", "TARP", "TEN", "WENT", "WET", "QUAR"]
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+
+  #testing on 7X7 grid
+  def test_7x7_grid(self):
+    grid = [
+      ["H", "A", "R", "B", "O", "R", "S"],
+      ["T", "R", "A", "V", "E", "L", "S"],
+      ["W", "I", "N", "T", "E", "R", "S"],
+      ["S", "N", "O", "W", "F", "A", "L"],
+      ["B", "R", "I", "S", "K", "S", "L"],
+      ["C", "O", "L", "D", "S", "E", "A"],
+      ["B", "E", "A", "C", "H", "S", "S"]
+    ]
+    dictionary = ["harbor", "travels", "winter", "snowfall", "brisk", "cold", "beach", "seas"]
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = ["HARBOR", "WINTER", "COLD", "BEACH"]
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+    
+  def test_13x13_grid_no_words_found(self):
+    """Test Boggle solver with a 13x13 grid and a dictionary where no words can be formed."""
+    grid = [
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"],
+        ["Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y"],
+        ["X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X", "Y", "X"]
+    ]
+    
+    dictionary = [
+        "HELLO", "WORLD", "PYTHON", "UNITTEST",
+        "OPENAI", "CHATGPT", "ALGORITHM", "DATA",
+        "STRUCTURE", "FUNCTION", "VARIABLE", "CLASS"
+    ]
+    
+    mygame = Boggle(grid, dictionary)
+    solution = [word.upper() for word in mygame.getSolution()]
+    expected = []
+    self.assertEqual(sorted(expected), sorted(solution))
 
 class TestSuite_Simple_Edge_Cases(unittest.TestCase):
   #ADD MANY SIMPLE TEST CASES
@@ -29,7 +92,7 @@ class TestSuite_Simple_Edge_Cases(unittest.TestCase):
     solution = mygame.getSolution()
     solution = [x.upper() for x in solution]
     expected = []
-		solution = sorted(solution)
+    solution = sorted(solution)
     expected = sorted(expected)
     self.assertEqual(expected, solution)
 
@@ -40,19 +103,88 @@ class TestSuite_Simple_Edge_Cases(unittest.TestCase):
     solution = mygame.getSolution()
     solution = [x.upper() for x in solution]
     expected = []
-		solution = sorted(solution)
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+  
+  # testing when there is no word in dictionary or no word is considered valid
+  def test_EmptyDictionary(self):
+    grid = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
+    dictionary = []  
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = []
+    solution = sorted(solution)
     expected = sorted(expected)
     self.assertEqual(expected, solution)
 
+  
+
 class TestSuite_Complete_Coverage(unittest.TestCase):
   #ADD MANY COMPLEXED TEST CASES
-  def test_case_1(self):  
-    self.assertEqual(True, True)
+
+  # no words from the dictionary can be formed
+  def test_NoWordsFromDictionary(self):
+    grid = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
+    dictionary = ["xyz", "mno", "pqr"]  
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = []  
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+  
+  # only a few words can be formed from the dictionary
+  def test_PartialWordsFromDictionary(self):
+    grid = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
+    dictionary = ["abc", "cfi", "xyz", "ghi"]  
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = ["ABC", "CFI", "GHI"]
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+
+  # All words from the dictionary can be formed
+  def test_AllWordsFromDictionary(self):
+    grid = [["A", "B", "C"],["D", "E", "F"],["G", "H", "I"]]
+    dictionary = ["abc", "cfi", "beh", "def", "ghi", "adg", "aei", "ceg"]
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [word.upper() for word in solution]
+    expected = ["ABC", "CFI", "BEH", "DEF", "GHI", "ADG", "AEI", "CEG"]
+    solution_sorted = sorted(solution)
+    expected_sorted = sorted(expected)
+    self.assertEqual(expected_sorted, solution_sorted)
+
 
 class TestSuite_Qu_and_St(unittest.TestCase):
-  #ADD QU AND ST TEST CASES
-  def test_case_1(self): 
-    self.assertEqual(True, True)
+  # Test case with grid containing both "QU" and "ST" as multi-letter cells
+  def test_GridWithMultipleMultiLetterCellsValidWords(self):
+    grid = [["St", "A", "R"], ["Qu", "E", "T"], ["F", "G", "H"]]  
+    dictionary = ["star", "start", "quest", "quiet", "rat", "hat", "set"] 
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = ["STAR", "START", "QUEST", "RAT"] 
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
+  
+  # Test case with more complex words formed from "QU" and "ST"
+  def test_GridWithMoreComplexWords(self):
+    grid = [["St", "A", "R"], ["Qu", "E", "T"], ["F", "G", "H"]]  
+    dictionary = ["stqufgearth", "startequfgh", "star", "start", "quest", "quiet", "rat", "hat", "set"] 
+    mygame = Boggle(grid, dictionary)
+    solution = mygame.getSolution()
+    solution = [x.upper() for x in solution]
+    expected = ["STAR", "START", "QUEST", "RAT", "STARTEQUFGH", "STQUFGEARTH"] 
+    solution = sorted(solution)
+    expected = sorted(expected)
+    self.assertEqual(expected, solution)
 
 if __name__ == '__main__':
 	unittest.main()
